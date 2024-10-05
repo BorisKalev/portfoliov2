1d7c61d71b5787e38c5f968e0f7c23dad42d984c
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-10-04 20:50:19 -0400

    made the sizing in different screen size better

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,20 +7,20 @@ export function Hero() {
   const { t } = useTranslation();
   return (
     <>
-      <LampContainer className="text-3xl">
+      <LampContainer className="relative text-3xl h-screen">
         <div className="max-w-2xl text-center font-poppins ">
-          <p className="relative sm-max:-top-8 sm-max:text-2xl bg-gradient-to-br from-slate-100 to-slate-500 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent">
+          <p className="relative 3xl:bottom-16 sm-max:text-2xl bg-gradient-to-br from-slate-100 to-slate-500 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent">
             {t("intro")}
           </p>
           <div className="relative top-7 flex justify-center items-center font-bold">
-            <CiLocationOn className="relative bottom-[2px]" />
-            <span className="ml-2 bg-gradient-to-br from-slate-100 to-slate-500 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent">
+            <CiLocationOn className="relative 3xl:bottom-12 bottom-[2px]" />
+            <span className="relative 3xl:bottom-12 ml-2 bg-gradient-to-br from-slate-100 to-slate-500 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent">
               {t("location")}
             </span>
           </div>
         </div>
         <a
-          className="relative text-sm top-24 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          className="relative  text-sm 3xl:top-12 top-24 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           href="mailto:boris_kalev_14@hotmail.com"
         >
           {t("button")}
